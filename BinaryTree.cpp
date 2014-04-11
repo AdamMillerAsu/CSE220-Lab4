1c7698c9bf8bea5334a527539f0a9562e85c5a5d
dpwong <daniel.p.wong2@gmail.com>
2014-04-10 22:25:54 -0700

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -7,15 +7,14 @@
 
 #include <string.h>
 #include "Token.h"
-#include <stdio.h>
 struct LineList
 {
 private:
 	int lineNumber;
 	struct LineList *next;
 public:
-	//getter functions
-	void setLineNumber(int lineNumber)
+	
+	void setLineNumber(int lineNumber) // setters
 	{
 		this->lineNumber = lineNumber;
 	}
@@ -23,14 +22,14 @@ public:
 	{
 		this->next = &next;
 	}
-	//setter functions
-	int getLineNumber()
+	
+	int getLineNumber() // getters
 	{
 		return lineNumber;
 	}
-	LineList getNext()
+	LineList* getNext()
 	{
-		return *next;
+		return next;
 	}
 
 	LineList(int line) // constructor
@@ -45,6 +44,7 @@ public:
 		next = NULL;
 	}
 };
+
 struct treeNode
 {
 private:
@@ -53,23 +53,83 @@ private:
 	struct treeNode *left;
 	struct treeNode *right;
 public:
-	void insertNode(Token *add, int lineNum)
+
+	void setLeft(treeNode *leftNode) // setters
 	{
-		if(add->getTokenString().compare(id->getTokenString()) == 0)
+		left = leftNode;
+	}
+	void setRight(treeNode *rightNode)
+	{
+		right = rightNode;
+	}
+
+	treeNode* getRight() // getters
+	{
+		return right;
+	}
+	treeNode* getLeft()
+	{
+		return left;
+	}
+
+	treeNode(Token *id, int lineNum) // constructor
+	{
+		LineList newNum = new LineList(lineNum);
+		line = &newNum;
+		left = NULL;
+		Right = NULL;
+		this->id = id;
+	}
+
+	~treeNode() // destructor
+	{
+		delete left;
+		left = NULL;
+		delete right;
+		right = NULL;
+		delete line;
+		line = NULL;
+		delete id;
+		id = NULL;
+	}
+
+	void insertNode(Token add, int lineNum)
+	{
+		if(add->getTokenString().compare(id->getTokenString()) == 0) // if string is the same
 		{
-			LineList *newNum = new LineList(lineNum);
+			LineList *newNum = new LineList(num);
 			LineList *temp = line;
-			while(&temp->getNext() != NULL)
+			while(&temp->getNext() != NULL) // add line number
 			{
-				temp = &temp->getNext();
+				&temp = temp->getNext();
 			}
 			temp->setNext(*newNum);
 		}
 		else
 		{
-			if(add->getTokenString().compare(id->getTokenString()) < 0)
+			if(add->getTokenString().compare(id->getTokenString()) < 0) // if string is less than
 			{
-
+				if(id->getLeft() == NULL) // add node if left is empty
+				{
+					treeNode newNode = new treeNode(&add, lineNum);
+					id->setLeft(newNode);
+				}
+				else // recursively call insertNode
+				{
+					left->insertNode(add, lineNum);
+				}
+			} // if string is greater than
+			else
+			{
+				if(id->getRight() == NULL)
+				{
+					treeNode newNode = treeNode(&add, lineNum);
+					id->setRight(newNode);
+				}
+				else
+				{
+					right->insertNode(add, lineNum);
+				}
 			}
 		}
 	}
