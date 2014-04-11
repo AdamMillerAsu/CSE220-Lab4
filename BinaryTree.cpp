b191495a7e1846c20ed9cd9ba16338ebffc28ea7
dpwong <daniel.p.wong2@gmail.com>
2014-04-10 23:30:31 -0700

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,140 +1,118 @@
 //
-//  main.cpp
+//  BinaryTree.cpp
 //  Lab4
 //
-//  Created by Daniel Wong, Adam Miller, and Mike Burgett
+//  Created by Daniel Wong and Adam Miller
 //
 
 #include <string.h>
 #include "Token.h"
-struct LineList
+#include "BinaryTree.h"
+LineList::void setLineNumber(int lineNumber) // setters
 {
-private:
-	int lineNumber;
-	struct LineList *next;
-public:
-	
-	void setLineNumber(int lineNumber) // setters
-	{
-		this->lineNumber = lineNumber;
-	}
-	void setNext(LineList next)
-	{
-		this->next = &next;
-	}
-	
-	int getLineNumber() // getters
-	{
-		return lineNumber;
-	}
-	LineList* getNext()
-	{
-		return next;
-	}
-
-	LineList(int line) // constructor
-	{
-		setLineNumber(line);
-		next = NULL;
-	}
-	
-	~LineList() // destructor
-	{
-		delete next;
-		next = NULL;
-	}
-};
-
-struct treeNode
+	this->lineNumber = lineNumber;
+}
+LineList::void setNext(LineList next)
 {
-private:
-	struct Token *id;
-	struct LineList *line;
-	struct treeNode *left;
-	struct treeNode *right;
-public:
+	this->next = &next;
+}
 
-	void setLeft(treeNode *leftNode) // setters
-	{
-		left = leftNode;
-	}
-	void setRight(treeNode *rightNode)
-	{
-		right = rightNode;
-	}
+LineList::int getLineNumber() // getters
+{
+	return lineNumber;
+}
+LineList::LineList* getNext()
+{
+	return next;
+}
+LineList::LineList(int line) // constructor
+{
+	setLineNumber(line);
+	next = NULL;
+}
 
+LineList::~LineList() // destructor
+{
+	delete next;
+	next = NULL;
+}
+
+TreeNode::void setLeft(treeNode *leftNode) // setters
+{
+	left = leftNode;
+}
+TreeNode::void setRight(treeNode *rightNode)
+{
+	right = rightNode;
+}
 	treeNode* getRight() // getters
+{
+	return right;
+}
+TreeNode::treeNode* getLeft()
+{
+	return left;
+}
+TreeNode::treeNode(Token *id, int lineNum) // constructor
+{
+	LineList *newNum;
+	newNum = new LineList(lineNum);
+	line = newNum;
+	left = NULL;
+	right = NULL;
+	this->id = id;
+}
+TreeNode::~treeNode() // destructor
+{
+	delete left;
+	left = NULL;
+	delete right;
+	right = NULL;
+	delete line;
+	line = NULL;
+	delete id;
+	id = NULL;
+}
+TreeNode::void insertNode(Token *add, int lineNum)
+{
+	if(add->getTokenString().compare(id->getTokenString()) == 0) // if string is the same
 	{
-		return right;
-	}
-	treeNode* getLeft()
-	{
-		return left;
-	}
-
-	treeNode(Token *id, int lineNum) // constructor
-	{
-		LineList *newNum;
+		LineList *newNum; 
 		newNum = new LineList(lineNum);
-		line = newNum;
-		left = NULL;
-		right = NULL;
-		this->id = id;
-	}
-
-	~treeNode() // destructor
-	{
-		delete left;
-		left = NULL;
-		delete right;
-		right = NULL;
-		delete line;
-		line = NULL;
-		delete id;
-		id = NULL;
-	}
-
-	void insertNode(Token *add, int lineNum)
-	{
-		if(add->getTokenString().compare(id->getTokenString()) == 0) // if string is the same
+		LineList *temp = line;
+		while(temp->getNext() != NULL) // add line number
 		{
-			LineList *newNum; 
-			newNum = new LineList(lineNum);
-			LineList *temp = line;
-			while(temp->getNext() != NULL) // add line number
-			{
-				temp = temp->getNext();
-			}
-			temp->setNext(*newNum);
+			temp = temp->getNext();
 		}
+		temp->setNext(*newNum);
+	}
+	else
+	{
+		if(add->getTokenString().compare(id->getTokenString()) < 0) // if string is less than
+		{
+			if(left == NULL) // add node if left is empty
+			{
+				treeNode *newNode;
+				newNode = new treeNode(add, lineNum);
+				left = newNode;
+			}
+			else // recursively call insertNode
+			{
+				left->insertNode(add, lineNum);
+			}
+		} // if string is greater than
 		else
 		{
-			if(add->getTokenString().compare(id->getTokenString()) < 0) // if string is less than
+			if(right == NULL)
 			{
-				if(left == NULL) // add node if left is empty
-				{
-					treeNode *newNode;
-					newNode = new treeNode(add, lineNum);
-					left = newNode;
-				}
-				else // recursively call insertNode
-				{
-					left->insertNode(add, lineNum);
-				}
-			} // if string is greater than
+				treeNode *newNode;
+				newNode = new treeNode(add, lineNum);
+				right = newNode;
+			}
 			else
 			{
-				if(right == NULL)
-				{
-					treeNode *newNode;
-					newNode = new treeNode(add, lineNum);
-					right = newNode;
-				}
-				else
-				{
-					right->insertNode(add, lineNum);
-				}
+				right->insertNode(add, lineNum);
 			}
 		}
 	}
-};
+}
