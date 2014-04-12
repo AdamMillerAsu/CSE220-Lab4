c4ddf3d220e9d73c2bf89d2f73f166a857ff4a66
dpwong <daniel.p.wong2@gmail.com>
2014-04-11 20:43:56 -0700

    Update BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -1,57 +1,46 @@
 //
-//  Scanner.h
+//  BinaryTree.h
 //  Lab4
 //
-//  Created by Bryce Holton.
+//  Created by Daniel Wong and Adam Miller
 //
 
-#ifndef Lab4_Scanner_h
-#define Lab4_Scanner_h
+#ifndef Lab4_BinaryTree_h
+#define Lab4_BinaryTree_h
 
-#include "common.h"
+#include <string.h>
 #include "Token.h"
-#include "Print.h"
 
-#define CHAR_TABLE_SIZE 256
-
-typedef enum
-{
-    LETTER, DIGIT, QUOTE, SPECIAL, EOF_CODE,
-}
-CharCode;
-
-class Scanner
+struct LineList
 {
 private:
-    /*********************
-     Private Variables for Scanner
-     Must be initialized in the constructor.
-     *********************/
-    Print print;
-    FILE *src_file;
-    char src_name[MAX_FILE_NAME_LENGTH];
-    char todays_date[DATE_STRING_LENGTH];
-    CharCode char_table[CHAR_TABLE_SIZE];  // The character table
-    char source_line[MAX_SOURCE_LINE_LENGTH];
-    char *line_ptr = NULL;
-    int line_number;
-    
-    bool getSourceLine(char source_buffer[]);
-    char getChar(char source_buffer[]);
-    void skipBlanks(char source_buffer[]);
-    void skipComment(char source_buffer[]);
-    void getWord(char *str, char *token_ptr, Token *tok);
-    void getNumber(char *str, char *token_ptr, Token *tok);
-    void getString(char *str, char *token_ptr, Token *tok);
-    void getSpecial(char *str, char *token_ptr, Token *tok);
-    void downshiftWord(char word[]);
-    bool isReservedWord(char *str, Token *tok);
-    
+	int lineNumber;
+	struct LineList *next;
 public:
-    Scanner(FILE *source_file, char source_name[], char date[], Print printer);
-    ~Scanner();
-    Token* getToken();
-
+	void setLineNumber(int lineNumber);
+	void setNext(LineList next);
+	int getLineNumber();
+	LineList* getNext();
+	LineList(int line);
+	~LineList();
 };
 
+struct TreeNode
+{
+private:
+	struct Token *id;
+	struct LineList *line;
+	struct TreeNode *left;
+	struct TreeNode *right;
+public:
+	void setLeft(TreeNode *leftNode);
+	void setRight(TreeNode *rightNode);
+	TreeNode* getRight();
+	TreeNode* getLeft();
+	Token* getId();
+	LineList* getLineList();
+	TreeNode(Token *id, int lineNum);
+	~TreeNode();
+	void insertNode(Token *add, int lineNum);
+};
 #endif
